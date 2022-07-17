b5cc658f31da32cb8591fc4fda19a2793b109da0
Fábio Júnior Nielsson Bastian <fabiobastian44@gmail.com>
2022-07-17 18:18:38 -0300

    up

--- aula11/ex002.js
+++ aula11/ex002.js
@@ -1,1 +1,6 @@
-console.log("dasdasd")
+var vel = 90;
+console.log(`A delocidade do seu carro é ${vel} Km/h`);
+if (vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO!!`);
+}
+console.log(`Dirija sempre com sinto de segurança.`);
---
+++ aula11/ex003.js
@@ -0,0 +1,8 @@
+var pais = "EUA"
+var brasil = "brasil"
+console.log(`Vivendo em ${pais}`)
+if(pais == brasil){
+    console.log(`Vc é brasileiro`)
+}else{
+    console.log(`Vc é estranjeiro`)
+}
