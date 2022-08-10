8a0539bc2eff3d71024afe9a763d8b2ea83908fa
Fábio Júnior Nielsson Bastian <fabiobastian44@gmail.com>
2022-08-10 20:20:24 -0300

    up

---
+++ Trabalho_Hyperpoems/script.js
@@ -0,0 +1,115 @@
+function traducao1(){
+    var t1 = document.querySelector("div#t1")
+    t1.innerHTML = "<h1>Wild nights, Emily Dickinson</h1> \
+    <p> \
+        Wild Nights, Wild Nights – !<br> \
+        Were I with thee<br> \
+        Wild nights – should be<br> \
+        Our luxury!<br> \
+    </p> \
+    <p> \
+        Futile – the Winds –<br> \
+        To a Heart in port –<br> \
+        Done with the Compass –<br> \
+        Done with the Chart – !<br> \
+    </p> \
+    <p> \
+        Rowing in Eden –<br> \
+        Ah, the Sea!<br> \
+        Might I but moor –<br> \
+        Tonight –<br> \
+        In Thee!<br> \
+    </p>"
+}
+
+
+function traducao2(){
+    var t2 = document.querySelector("div#t2")
+    t2.innerHTML = "<h1>Below, da Harriet Beecher Stowe</h1>  \
+    <p> \
+        Loudly sweep the winds of autumn<br> \
+        O'er that lone, beloved grave,<br> \
+        Where we laid those sunny ringlets,<br> \
+        When those blue eyes set like stars,<br> \
+        Leaving us to outer darkness.<br> \
+        O the longing and the aching!<br> \
+        O the sere deserted grave!<br> \
+    </p> \
+    <p> \
+        Let the grass turn brown upon thee,<br> \
+        Brown and withered like our dreams!<br> \
+        Let the wind moan through the pine-trees<br> \
+        With a dreary, dirge-like whistle,<br> \
+        Sweep the dead leaves on its bosom,—<br> \
+        Moaning, sobbing through the branches,<br> \
+        Where the summer laughed so gayly.<br> \
+    </p> \
+    <p> \
+        He is gone, our boy of summer,—<br> \
+        Gone the light of his blue eyes,<br> \
+        Gone the tender heart and manly,<br> \
+        Gone the dreams and the aspirings,—<br> \
+        Nothing but the mound remaineth,<br> \
+        And the aching in our bosoms,<br> \
+        Ever aching, ever throbbing:<br> \
+        Who shall bring it unto rest?<br> \
+    </p>"
+}
+
+
+function traducao3(){
+    var t3 = document.querySelector("div#t3")
+    t3.innerHTML = "<h2>The Higher Brotherhood - Madison Julius Cawein</h2> \
+    <p> \
+        To come in touch with mysteries<br> \
+        Of beauty idealizing Earth,<br> \
+        Go seek the hills, grown old with trees,<br> \
+        The old hills wise with death and birth.<br> \
+    </p> \
+    <p> \
+        There you may hear the heart that beats<br> \
+        In streams, where music has its source;<br> \
+        And in wild rocks of green retreats<br> \
+        Behold the silent soul of force.<br> \
+    </p> \
+    <p> \
+        Above the love that emanates <br> \
+        From human passion, and reflects <br> \
+        The flesh, must be the love that waits<br> \
+        On Nature, whose high call elects<br> \
+    </p> \
+    <p> \
+        None to her secrets save the few<br> \
+        Who hold that facts are far less real<br> \
+        Than dreams, with which all facts indue<br> \
+        Themselves approaching the Ideal.<br> \
+    </p>"
+}
+
+
+function traducao4(){
+    var t4 = document.querySelector("div#t4")
+    t4.innerHTML = "<h1>A Jelly-Fish - Marianne Moore</h1> \
+    <p> \
+        Visible, invisible,<br> \
+        A fluctuating charm,<br> \
+        An amber-colored amethyst<br> \
+        Inhabits it; your arm<br> \
+        Approaches, and<br> \
+        It opens and<br> \
+        It closes;<br> \
+        You have meant<br> \
+        To catch it,<br> \
+        And it shrivels;<br> \
+        You abandon<br> \
+        Your intent—<br> \
+        It opens, and it<br> \
+        Closes and you<br> \
+        Reach for it—<br> \
+        The blue<br> \
+        Surrounding it<br> \
+        Grows cloudy, and<br> \
+        It floats away<br> \
+        From you.<br> \
+    </p>"
+}
