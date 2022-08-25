2990444f30d5550660e864ea9e6b11cecf9e081f
Fábio Júnior Nielsson Bastian <fabiobastian44@gmail.com>
2022-08-25 20:49:21 -0300

    up

--- Trabalho_Hyperpoems/script.js
+++ Trabalho_Hyperpoems/script.js
@@ -1,115 +1,121 @@
 function traducao1(){
     var t1 = document.querySelector("div#t1")
-    t1.innerHTML = "<h1>Wild nights, Emily Dickinson</h1> \
+    t1.innerHTML = "<h1>Ferozes Noites, Emily Dickinson</h1> \
     <p> \
-        Wild Nights, Wild Nights – !<br> \
-        Were I with thee<br> \
-        Wild nights – should be<br> \
-        Our luxury!<br> \
+    Ferozes noites, Ferozes noites - !<br> \
+    Eu estava com você<br> \
+    Ferozes noites - como devem ser<br> \
+    Nosso luxo!<br> \
     </p> \
     <p> \
-        Futile – the Winds –<br> \
-        To a Heart in port –<br> \
-        Done with the Compass –<br> \
-        Done with the Chart – !<br> \
+    Vazio – os Ventos -<br> \
+    Para um coração no porto -<br> \
+    Feito com a bússola -<br> \
+    Feito com o gráfico - !<br> \
     </p> \
     <p> \
-        Rowing in Eden –<br> \
-        Ah, the Sea!<br> \
-        Might I but moor –<br> \
-        Tonight –<br> \
-        In Thee!<br> \
+    Remando no Éden -<br> \
+    Ah, o Mar!<br> \
+    E se eu pudesse -<br> \
+    Esta noite -<br> \
+    Em ti me amarrar!<br> \
     </p>"
 }
 
 
 function traducao2(){
     var t2 = document.querySelector("div#t2")
-    t2.innerHTML = "<h1>Below, da Harriet Beecher Stowe</h1>  \
+    t2.innerHTML = "<h1>Abaixo - Harriet Beecher Stowe</h1> \
     <p> \
-        Loudly sweep the winds of autumn<br> \
-        O'er that lone, beloved grave,<br> \
-        Where we laid those sunny ringlets,<br> \
-        When those blue eyes set like stars,<br> \
-        Leaving us to outer darkness.<br> \
-        O the longing and the aching!<br> \
-        O the sere deserted grave!<br> \
+        Varra ruidosamente os ventos do outono<br> \
+        Sobre aquela sepultura solitária e amada,<br> \
+        Onde colocamos aqueles cachos ensolarados,<br> \
+        Quando aqueles olhos azuis se põem como estrelas,<br> \
+        Deixando-nos nas trevas exteriores.<br> \
+        Ó saudade e dor!<br> \
+        Ó sepultura deserta e deserta!<br> \
     </p> \
     <p> \
-        Let the grass turn brown upon thee,<br> \
-        Brown and withered like our dreams!<br> \
-        Let the wind moan through the pine-trees<br> \
-        With a dreary, dirge-like whistle,<br> \
-        Sweep the dead leaves on its bosom,—<br> \
-        Moaning, sobbing through the branches,<br> \
-        Where the summer laughed so gayly.<br> \
+        Deixe a grama ficar marrom sobre ti,<br> \
+        Castanhos e murchos como os nossos sonhos!<br> \
+        Deixe o vento gemer pelos pinheiros<br> \
+        Com um assobio lúgubre, semelhante a um canto fúnebre,<br> \
+        Varra as folhas mortas em seu seio,<br> \
+        Gemendo, soluçando entre os galhos,<br> \
+        Onde o verão ria tão alegremente.<br> \
     </p> \
     <p> \
-        He is gone, our boy of summer,—<br> \
-        Gone the light of his blue eyes,<br> \
-        Gone the tender heart and manly,<br> \
-        Gone the dreams and the aspirings,—<br> \
-        Nothing but the mound remaineth,<br> \
-        And the aching in our bosoms,<br> \
-        Ever aching, ever throbbing:<br> \
-        Who shall bring it unto rest?<br> \
+        Ele se foi, nosso menino do verão,—<br> \
+        Foi-se a luz de seus olhos azuis,<br> \
+        Foi-se o coração terno e viril,<br> \
+        Acabaram-se os sonhos e as aspirações,—<br> \
+        Nada além do monte permanece,<br> \
+        E a dor em nossos seios,<br> \
+        Sempre dolorido, sempre latejando:<br> \
+        Quem o trará ao descanso?<br> \
     </p>"
 }
 
 
 function traducao3(){
     var t3 = document.querySelector("div#t3")
-    t3.innerHTML = "<h2>The Higher Brotherhood - Madison Julius Cawein</h2> \
+    t3.innerHTML = "<h2>A Irmandade Superior - Madison Julius Cawein</h2> \
     <p> \
-        To come in touch with mysteries<br> \
-        Of beauty idealizing Earth,<br> \
-        Go seek the hills, grown old with trees,<br> \
-        The old hills wise with death and birth.<br> \
+        Para ficar em contato com os mistérios<br> \
+        Da beleza idealizando a Terra,<br> \
+        Vá procurar as colinas, envelhecido com as árvores,<br> \
+        As velhas colinas são sábias com morte e nascimento.<br> \
     </p> \
     <p> \
-        There you may hear the heart that beats<br> \
-        In streams, where music has its source;<br> \
-        And in wild rocks of green retreats<br> \
-        Behold the silent soul of force.<br> \
+        Lá você provavelmente escutará o coração que bate<br> \
+        Em fluxos, onde a música tem sua origem<br> \
+        E em pedras selvagens de verdes retiros<br> \
+        Observa a silenciosa alma da força.<br> \
     </p> \
     <p> \
-        Above the love that emanates <br> \
-        From human passion, and reflects <br> \
-        The flesh, must be the love that waits<br> \
-        On Nature, whose high call elects<br> \
+        Acima do amor que emana<br> \
+        Da paixão humana, e reflete<br> \
+        A carne, deve ser o amor que aguarda<br> \
+        Na natureza, onde os grandes elege<br> \
     </p> \
     <p> \
-        None to her secrets save the few<br> \
-        Who hold that facts are far less real<br> \
-        Than dreams, with which all facts indue<br> \
-        Themselves approaching the Ideal.<br> \
+        Nenhum para seus segredos salva o pouco<br> \
+        Que afirma que os fatos são bem menos reais<br> \
+        Então sonhos, de onde todos fatos devem<br> \
+        Aproximar do Ideal. <br> \
     </p>"
 }
 
 
 function traducao4(){
     var t4 = document.querySelector("div#t4")
-    t4.innerHTML = "<h1>A Jelly-Fish - Marianne Moore</h1> \
+    t4.innerHTML = "<h1>Uma Medusa - Marianne Moore</h1> \
     <p> \
-        Visible, invisible,<br> \
-        A fluctuating charm,<br> \
-        An amber-colored amethyst<br> \
-        Inhabits it; your arm<br> \
-        Approaches, and<br> \
-        It opens and<br> \
-        It closes;<br> \
-        You have meant<br> \
-        To catch it,<br> \
-        And it shrivels;<br> \
-        You abandon<br> \
-        Your intent—<br> \
-        It opens, and it<br> \
-        Closes and you<br> \
-        Reach for it—<br> \
-        The blue<br> \
-        Surrounding it<br> \
-        Grows cloudy, and<br> \
-        It floats away<br> \
-        From you.<br> \
+        Visível, invisível,<br> \
+        Um encanto no espaço,<br> \
+        Uma ametista de cor âmbar<br> \
+        Habita; seu braço<br> \
+        Abordagens, e<br> \
+        Ele abre e<br> \
+        Isso fecha;<br> \
+        Você quis dizer<br> \
+        Para pegá-lo,<br> \
+        E murcha;<br> \
+        Você abandona<br> \
+        Sua intenção—<br> \
+        Ele abre, e<br> \
+        Fecha, e você,<br> \
+        Alcança-o—<br> \
+        O azul<br> \
+        Ao seu redor<br> \
+        Fica nublado, e<br> \
+        Flutua para longe<br> \
+        De você.<br> \
     </p>"
+}
+
+
+function pegadinha(){
+    var pegadinha = document.querySelector("div#container")
+    pegadinha.innerHTML = "<h1>VC CAIU NA PEGADINHA</h1>"
 }
